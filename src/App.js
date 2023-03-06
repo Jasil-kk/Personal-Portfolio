6d6bbc1699865b9c489d8e8414d6cf77d0e0947b
Jasil-kk <jasikk2522@gmail.com>
2023-03-06 12:12:48 +0530

    projects,projectCard,contact added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { NavBar } from './Components/NavBar';
 import { Banner } from './Components/Banner';
 import { Skills } from './Components/Skills';
+import { Projects } from './Components/Projects';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -11,6 +12,7 @@ function App() {
       <NavBar />
       <Banner />
       <Skills />
+      <Projects />
     </div>
   );
 }
