87272e601e996a0b7e5b7a77485def53d3d4925f
Jasil-kk <jasikk2522@gmail.com>
2023-04-14 12:41:44 +0530

    logo changed

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../Assets/img/logo.svg';
+import logo from '../Assets/Logo.svg';
 import navIcon1 from '../Assets/img/nav-icon1.svg';
 import navIcon2 from '../Assets/img/nav-icon2.svg';
 import navIcon3 from '../Assets/img/nav-icon3.svg';
@@ -50,7 +50,7 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.linkedin.com/in/jasil-kk-92119b269/"><img src={navIcon1} alt="" /></a>
                 <a href="#"><img src={navIcon2} alt="" /></a>
                 <a href="https://instagram.com/_jasil_kk?igshid=ZDdkNTZiNTM="><img src={navIcon3} alt="" /></a>
               </div>
