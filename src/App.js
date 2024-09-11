a03a25617b88375e4038e0af0b2f98e2737b4d68
Jasil-kk <jasikk2522@gmail.com>
2024-09-11 13:18:14 +0530

    changed the social media links

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
-import './App.css';
-import { NavBar } from './Components/NavBar';
-import { Banner } from './Components/Banner';
-import { Skills } from './Components/Skills';
-import { Projects } from './Components/Projects';
-import { Footer } from './Components/Footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Contact } from './Components/Contact';
-import { useEffect, useState } from 'react';
-import Loading from './Components/Loading/Loading';
+import "./App.css";
+import { NavBar } from "./Components/NavBar";
+import { Banner } from "./Components/Banner";
+import { Skills } from "./Components/Skills";
+import { Projects } from "./Components/Projects";
+import { Footer } from "./Components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Contact } from "./Components/Contact";
+import { useEffect, useState } from "react";
+import Loading from "./Components/Loading/Loading";
 
 function App() {
-
   const [loading, setLoading] = useState(false);
 
-useEffect(()=>{
-    setLoading(true)
-    setTimeout(()=>{
-        setLoading(false)
-    },1500)
-},[]);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
 
   return (
     <>
-    {loading ? <Loading/>
-     :
-    <div className="App">
-      <NavBar />
-      <Banner />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
-  }
-  </>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="App">
+          <NavBar />
+          <Banner />
+          <Skills />
+          <Projects />
+          <Contact />
+          <Footer />
+        </div>
+      )}
+    </>
   );
 }
 
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,7 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
-import logo from "../Assets/img/logo.svg";
+// import logo from "../Assets/img/logo.svg";
+import logo from "../Assets/Logo.svg";
 import navIcon1 from "../Assets/img/nav-icon1.svg";
 import navIcon2 from "../Assets/img/nav-icon2.svg";
 import navIcon3 from "../Assets/img/nav-icon3.svg";
@@ -16,9 +17,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/jasil-kk/"><img src={navIcon1} alt="Icon" /></a>
               <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="https://instagram.com/_jasil_kk?igshid=ZDdkNTZiNTM="><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.instagram.com/jasil_kk_/"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
